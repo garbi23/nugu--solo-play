f3b86220f36c780ebb7f753017687b24807a0e89
garbi23 <garbi23@naver.com>
2019-08-07 01:03:33 +0900

    nugu

--- nugu/index.js
+++ nugu/index.js
@@ -41,7 +41,7 @@ function parsingsrvalue(){
 
     var str = String(results)
     srvalue = 10
-    console.log(str)
+    console.log(srvalue)
     if(srvalue <= 30){
         srstat = '물이 부족합니다! 어서 물을 주세요!'
     }else if(srvalue > 30 || srvalue < 80){
