80f510bc0fe2d2b94ab433a4d52501cbe171ecdc
garbi23 <garbi23@naver.com>
2019-08-07 02:47:18 +0900

    nugu

--- nugu/index.js
+++ nugu/index.js
@@ -172,7 +172,7 @@ class NPKRequest {
        if(err){throw err}
         
         srvalue = results
-  
+        
         if(srvalue <= 30){
           srstat = '물이 부족합니다! 어서 물을 주세요!'
         }else if(srvalue > 30 || srvalue < 80){
@@ -180,12 +180,11 @@ class NPKRequest {
        }else{
           srstat = '물이 충분합니다!'
        }
-  
-        console.log(srstat)
-        console.log(srvalue)
-
-        npkResponse.setOutputsrvaluePar()
      });
+
+     console.log(srstat)
+     console.log(srvalue)
+     npkResponse.setOutputsrvaluePar()
     break    
     }
   }
