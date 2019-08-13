56955ead34b7d9d24f1b6344073a947722a64671
garbi23 <garbi23@naver.com>
2019-08-13 13:44:49 +0900

    nugu

--- nugu/rcp.js
+++ nugu/rcp.js
@@ -14,28 +14,29 @@ rcp.rcpgmae = function(choice1){
     }
         console.log("Computer: " + computerChoice);
 
-    if (choice1 === computerChoice){
+    if (choice1 == computerChoice){
         return "저는" + computerChoice + "결과는 무승부에요!";
     }
 
-        if (choice1 === "바위"){
-            if (computerChoice === "가위"){
-                return "저는 " + computerChoice + "! ,제가 이겼어용 헤헷!";
-        }else {
-            return "저는 " + computerChoice + "! ,주인님이 이겼어요, 유,유,유";
-        }
-    }
-
-    if (choice1 === "보자기"){
-        if (computerChoice === "가위"){
+        if (choice1 == "바위"){
+            if (computerChoice == "가위"){
+                
+                return "저는 " + computerChoice + "! ,주인님이 이겼어요, 유,유,유";
+        }else{
             return "저는 " + computerChoice + "! ,제가 이겼어용 헤헷!";
-        }else {
+        }
+    }
+
+    if (choice1 == "보자기"){
+        if (computerChoice == "가위"){
+            return "저는 " + computerChoice + "! ,제가 이겼어용 헤헷!";
+        }else{
             return "저는 " + computerChoice + "! ,주인님이 이겼어요, 유,유,유";
         }
     }
 
-    if (choice1 === "가위"){
-        if (computerChoice === "바위"){
+    if (choice1 == "가위"){
+        if (computerChoice == "바위"){
             return "저는 " + computerChoice + "! ,제가 이겼어용 헤헷!";
         }else {
             return "저는 " + computerChoice + "! ,주인님이 이겼어요, 유,유,유";
