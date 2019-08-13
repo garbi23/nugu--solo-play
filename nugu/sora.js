80f1fece8636b64e7dcb2fc79e5082d86097deaf
garbi23 <garbi23@naver.com>
2019-08-13 15:17:34 +0900

    nugu

--- nugu/sora.js
+++ nugu/sora.js
@@ -4,10 +4,10 @@ let sora = {}
 sora.soragame = function(){
 
     if (computerChoice < 0.34) {
-        return computerChoice = "안,돼";
+        return computerChoice = "아니,안,돼";
 
     }else if(computerChoice <= 0.67) {
-        return computerChoice = "돼";
+        return computerChoice = "응,돼";
 
     } else {
         return computerChoice = "몰,라";
