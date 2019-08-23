56c9969d32226c895414120023580ca58f9bda1c
garbi23 <garbi23@naver.com>
2019-08-23 17:53:10 +0900

    nugu

--- nugu/dbpost.js
+++ nugu/dbpost.js
@@ -1,33 +1,25 @@
-var querystring = require('querystring');
-var http = require('http');
+var mysql = require('mysql')
 let dbpost = {}
 
+
+var connection = mysql.createConnection({
+    host : 'mynugusql.c9utrsxn9yo6.ap-northeast-2.rds.amazonaws.com',
+    user : 'root',
+    password : 'xodnsqkqh233',
+    database : 'nugudb'
+  });
+  
+  connection.connect()
+  
+
 dbpost.Postcode = function(codestring) {
-  // Build the post string from an object
-  var post_data = querystring.stringify({
-        'soilwater' : codestring,
-        'temp' : codestring,
-        'humi' : codestring
-  });
 
-  // An object of options to indicate where to post to
-  var post_options = {
-      host: 'http://15.164.91.18',
-      port: '80',
-      path: '/index.php',
-      method: 'POST',
-  };
-
-  // Set up the request
-  var post_req = http.request(post_options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
-      });
-  });
-  // post the data
-  post_req.write(post_data);
-  post_req.end();
+    var Post_Query = "UPDATE sensor SET srvalue = '"+codestring+"' WHERE srkind = 'temp'";
+    connection.query(Post_Query, function(err, rows, fields){
+        if(err){
+          throw err
+        }
+     });
 }
 
 module.exports = dbpost;
