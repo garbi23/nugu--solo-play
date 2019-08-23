38599b0baa64bd3edc267483bd89e55d9f23c63c
garbi23 <garbi23@naver.com>
2019-08-23 16:47:12 +0900

    nugu

--- nugu/index.js
+++ nugu/index.js
@@ -75,7 +75,7 @@ if(gamekind == 1){
         number = drink
     
       }else if(mod.modnumber() == 2){
-        number = '총 ' + number +'번 까지' + time + '초로 삼육구를 클리어 하셨습니다! 랭킹은 1위 입니다!'
+        number = '총 ' + number +'번 까지 ' + time + '초로 삼육구를 클리어 하셨습니다! 랭킹은 1위 입니다!'
         clearInterval(intervalObj)
         time = 0
       }else{
