a4cf0577c4ad44ca83f0a15f914fc3b5b2069426
garbi23 <garbi23@naver.com>
2019-08-23 20:44:50 +0900

    nugu

--- nugu/rankdb.js
+++ nugu/rankdb.js
@@ -15,8 +15,7 @@ connection.connect()
 
 
 soil.value = function(num){
-    var Dupli_Query = "INSERT INTO tsnrank(id, score) values(default,"+ num +");"+
-    "SELECT t.score, (SELECT COUNT(*) FROM  tsnrank WHERE score >= t.score) AS rank FROM tsnrank  t WHERE score = '"+num+"';";
+    var Dupli_Query = "INSERT INTO tsnrank(id, score) values(default,"+ num +");"
     connection.query(Dupli_Query, function(err, rows, fields){
         if(err){
           throw err
