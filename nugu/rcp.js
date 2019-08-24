49baf04bf7682c0810f28cdd78833ad9a69a4244
garbi23 <garbi23@naver.com>
2019-08-24 16:22:10 +0900

    nugu

--- nugu/rcp.js
+++ nugu/rcp.js
@@ -3,55 +3,55 @@ let alcol = 0
 let result = 0
 var mod = require("./mod")
 
-function alresult(number){
+function alresult(number, computerChoice){
 
 if(mod.modnumber() == 1){
     if(alcol >= 3){
         if(number == 1){
-            result = "후헤헤헹 뭐양... , 저능 보자깅! ,주인님이 이겼어요, 한잔 할게용...";
+            result = "후헤헤헹 뭐양... , 저능 "+computerChoice+"! ,주인님이 이겼어요, 한잔 할게용...";
         }else if(number == 2){
-            result = "하하하! 주인님이 졌어용!... , 저능 보자깅! ,한잔 먹어랏!";
+            result = "하하하! 주인님이 졌어용!... , 저능 "+computerChoice+"! ,한잔 먹어랏!";
         }else if(number == 3){
-            result = "흐극흐극흐극 뭐양... , 저능 가윙! ,주인님이 이겼어요, 한잔 할겡...";
+            result = "흐극흐극흐극 뭐양... , 저능 "+computerChoice+"! ,주인님이 이겼어요, 한잔 할겡...";
         }else if(number == 4){
-            result = "푸헤헤헤헤! 주인님이 졌어용!... , 저능 가윙! ,한잔 원샷 먹어야징!";
+            result = "푸헤헤헤헤! 주인님이 졌어용!... , 저능 "+computerChoice+"! ,한잔 원샷 먹어야징!";
         }else if(number == 5){
-            result = "라라라라! 주인님이 졌어용!... , 저능 바윙! ,한잔 가랏!";
+            result = "라라라라! 주인님이 졌어용!... , 저능 "+computerChoice+"! ,한잔 가랏!";
         }else if(number == 6){
-            result = "흥흥 뭐양... , 저능 바윙! ,주인님이 이겼어요, 한잔 먹어야겠당...";
+            result = "흥흥 뭐양... , 저능 "+computerChoice+"! ,주인님이 이겼어요, 한잔 먹어야겠당...";
         }else if(number == 7){
             result = "어라라라? 무승부넹.. 한번더하장!";
         }
     }else{
         if(number == 1){
-            result = "저는 보자기! ,주인님이 이겼어요, 한잔 할게요!";
+            result = "저는 "+computerChoice+"! ,주인님이 이겼어요, 한잔 할게요!";
         }else if(number == 2){
-            result = "저는 보자기! ,제가 이겼어용 헤헷! 한잔 하시죠!";
+            result = "저는 "+computerChoice+"! ,제가 이겼어용 헤헷! 한잔 하시죠!";
         }else if(number == 3){
-            result = "저는 가위! ,주인님이 이겼어요, 한잔 원샷 해볼게요!";
+            result = "저는 "+computerChoice+"! ,주인님이 이겼어요, 한잔 원샷 해볼게요!";
         }else if(number == 4){
-            result = "저는 가위! ,제가 이겼어용 헤헷!, 한잔 원샷 가시죠!";
+            result = "저는 "+computerChoice+"! ,제가 이겼어용 헤헷!, 한잔 원샷 가시죠!";
         }else if(number == 5){
-            result = "저는 바위! ,제가 이겼어용 헤헷!, 한잔 달려요!";
+            result = "저는 "+computerChoice+"! ,제가 이겼어용 헤헷!, 한잔 달려요!";
         }else if(number == 6){
-            result = "저는 바위! ,주인님이 이겼어요, 한잔 달려볼게요!";
+            result = "저는 "+computerChoice+"! ,주인님이 이겼어요, 한잔 달려볼게요!";
         }else if(number == 7){
             result = "결과는 무승부에요! 다음번에는 제가 이겨요!";
         }
     }
 }else{
         if(number == 1){
-            result = "저는 보자기! ,주인님이 이겼어요,";
+            result = "저는 "+computerChoice+"! ,주인님이 이겼어요,";
         }else if(number == 2){
-            result = "저는 보자기! ,제가 이겼어용 헤헷!";
+            result = "저는 "+computerChoice+"! ,제가 이겼어용 헤헷!";
         }else if(number == 3){
-            result = "저는 가위! ,주인님이 이겼어요,";
+            result = "저는 "+computerChoice+"! ,주인님이 이겼어요,";
         }else if(number == 4){
-            result = "저는 가위! ,제가 이겼어용 헤헷!";
+            result = "저는 "+computerChoice+"! ,제가 이겼어용 헤헷!";
         }else if(number == 5){
-            result = "저는 바위! ,제가 이겼어용 헤헷!";
+            result = "저는 "+computerChoice+"! ,제가 이겼어용 헤헷!";
         }else if(number == 6){
-            result = "저는 바위! ,주인님이 이겼어요,";
+            result = "저는 "+computerChoice+"! ,주인님이 이겼어요,";
         }else if(number == 7){
             result = "결과는 무승부에요! 다음번에는 제가 이겨요!!";
         }
@@ -71,17 +71,31 @@ rcp.resetal = function(){
 }
 
 
+
+
 rcp.rcpgmae = function(choice1){
     var computerChoice = Math.random();
 
     if (computerChoice < 0.34) {
-        computerChoice = "바위";
+
+        if(alcol >= 3){
+            computerChoice = "바윙";
+        }else{
+            computerChoice = "바위";
+        }
 
     }else if(computerChoice <= 0.67) {
-        computerChoice = "보자기";
-
+        if(alcol >= 3){
+            computerChoice = "보자깅";
+        }else{
+            computerChoice = "보자기";
+        }         
     } else {
-        computerChoice = "가위";
+        if(alcol >= 3){
+            computerChoice = "가윙";
+        }else{
+            computerChoice = "가위";
+        }   
     }
         console.log("Computer: " + computerChoice);
 
@@ -92,34 +106,34 @@ rcp.rcpgmae = function(choice1){
 
     if (choice1 == "바위"){
         if (computerChoice == "보자기"){ 
-            alresult(2)
+            alresult(2,computerChoice)
             return result
         }else{
             rcpmod()
             console.log(alcol);
-            alresult(1)
+            alresult(1,computerChoice)
             return result
         }
     }
     if (choice1 == "보자기" || choice1 == "보"){
         if (computerChoice == "가위"){
-            alresult(4)
+            alresult(4,computerChoice)
             return result
         }else{
             rcpmod()
             console.log(alcol);
-            alresult(3)
+            alresult(3,computerChoice)
             return result
         }
     }
     if (choice1 == "가위"){
         if (computerChoice == "바위"){
-            alresult(5)
+            alresult(5,computerChoice)
             return result
         }else {
             rcpmod()
             console.log(alcol);
-            alresult(6)
+            alresult(6,computerChoice)
             return result
         }
     }
