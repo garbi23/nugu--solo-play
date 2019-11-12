adac284f5b2e056ef4e5618e377ed2401d9209ae
garbi23 <garbi23@naver.com>
2019-11-12 16:36:39 +0900

    Update rankdb.js

--- nugu/rankdb.js
+++ nugu/rankdb.js
@@ -8,7 +8,7 @@ let score = 0
 var connection = mysql.createConnection({
   host : 'mynugusql.c9utrsxn9yo6.ap-northeast-2.rds.amazonaws.com',
   user : 'root',
-  password : 'xodnsqkqh233',
+  password : '**********',
   database : 'NuguRank'
 });
 
